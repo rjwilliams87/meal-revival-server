16b71032d07d1991e676b91c20c4c4758a237cdb
Ryan Williams <ryanjacobwilliams@gmail.com>
2019-01-22 22:51:27 -0600

    added travis

---
+++ test/test-server.js
@@ -0,0 +1,8 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { app } = require("../server");
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe("API", function() {});
